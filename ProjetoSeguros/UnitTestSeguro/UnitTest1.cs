d2fe4aa59f2a926f46f83c0219cb727aeaca3e7f
Mirian de Oliveira <mio.liv@hotmail.com>
2019-01-20 19:30:42 -0200

    teste unitarios

--- ProjetoSeguros/UnitTestSeguro/UnitTest1.cs
+++ ProjetoSeguros/UnitTestSeguro/UnitTest1.cs
@@ -8,9 +8,23 @@ namespace UnitTestSeguro
     public class UnitTest1
     {
         [TestMethod]
-        public void TestarCPFCliente()
+        public void TestarCPFValido()
         {
             Seguro s = new Seguro() {
+                id = 1,
+                indTipo = 3,
+                numCliente = "834.284.690-38",
+                objetoSegurado = "672.175.710-90"
+            };
+
+            Assert.AreEqual(true, Utilitarios.ValidaCPF(s.numCliente), "Número do cliente inválido");
+        }
+
+        [TestMethod]
+        public void TestarCPFInvalido()
+        {
+            Seguro s = new Seguro()
+            {
                 id = 1,
                 indTipo = 3,
                 numCliente = "098.098.098-09",
@@ -21,7 +35,7 @@ namespace UnitTestSeguro
         }
 
         [TestMethod]
-        public void TestarCNPJ()
+        public void TestarCNPJValido()
         {
             Seguro s = new Seguro()
             {
@@ -32,7 +46,21 @@ namespace UnitTestSeguro
             };
 
             Assert.AreEqual(true, Utilitarios.ValidaCNPJ(s.numCliente), "Número do cliente inválido");
-        }        
+        }
+
+        [TestMethod]
+        public void TestarCNPJInvalido()
+        {
+            Seguro s = new Seguro()
+            {
+                id = 1,
+                indTipo = 3,
+                numCliente = "00.009.232/9991-20",
+                objetoSegurado = "672.175.710-90"
+            };
+
+            Assert.AreEqual(true, Utilitarios.ValidaCNPJ(s.numCliente), "Número do cliente inválido");
+        }
 
     }
 }
