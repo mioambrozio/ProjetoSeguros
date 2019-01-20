f9e0b690884aed289ee06cc3789e1916bf120d89
Mirian de Oliveira <mio.liv@hotmail.com>
2019-01-20 19:26:48 -0200

    adicionado testes unitarios

---
+++ ProjetoSeguros/UnitTestSeguro/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSeguro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSeguro")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08e8ff21-e6a4-4bbf-b323-2fd8f81766e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjetoSeguros/UnitTestSeguro/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetoSeguros.Models;
+
+namespace UnitTestSeguro
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestarCPFCliente()
+        {
+            Seguro s = new Seguro() {
+                id = 1,
+                indTipo = 3,
+                numCliente = "098.098.098-09",
+                objetoSegurado = "672.175.710-90"
+            };
+
+            Assert.AreEqual(true, Utilitarios.ValidaCPF(s.numCliente), "Número do cliente inválido");
+        }
+
+        [TestMethod]
+        public void TestarCNPJ()
+        {
+            Seguro s = new Seguro()
+            {
+                id = 1,
+                indTipo = 3,
+                numCliente = "98.896.295/0001-20",
+                objetoSegurado = "672.175.710-90"
+            };
+
+            Assert.AreEqual(true, Utilitarios.ValidaCNPJ(s.numCliente), "Número do cliente inválido");
+        }        
+
+    }
+}
